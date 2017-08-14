696b06ee4b8674ac8e3ebb6f71b4cdd0605fcf23
Mitali Pattani <sonarmitali19@gmail.com>
2017-08-14 12:54:39 -0700

    Initial commit

---
+++ app/src/main/java/com/ciccc_cirac/bookdatabsedemo/Book.java
@@ -0,0 +1,48 @@
+package com.ciccc_cirac.bookdatabsedemo;
+
+/**
+ * Created by CICCC-CIRAC on 2017-08-14.
+ */
+
+public class Book {
+    private int id;
+    private String title, author;
+
+    public Book(String title, String author)
+    {
+        this.title = title;
+        this.author = author;
+    }
+
+    //getter & setter Methods
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    @Override
+    public String toString()
+    {
+        return  "BOOK [ id = " + id +" ,title = "+
+            title + " ,Author  = "+author + " ]";
+    }
+}
